b264f5d800d2949d30ae95fbf7d919c0513f2c31
Neal Geilen <info@nealgeilen.nl>
2023-12-05 20:54:55 +0100

    TEst

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-search_dir=`cd explainers && ls *.md`
-for entry in "$search_dir"
+search_dir=`cd explainers && ls *.md`for entry in "$search_dir"
 do
   name=${entry::${#entry}-3}
-  echo "/explained/$name/"
   exec npx slidev build "explainers/$entry" --out "./../dist/$name" --base /
-  echo "$entry"
 done
