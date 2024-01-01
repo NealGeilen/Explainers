c8a19cae40b35e37ffe06ce93a237f25cc3a8d7f
Neal Geilen <info@nealgeilen.nl>
2024-01-02 00:51:20 +0100

    prettier changes

--- slidev/.eslintrc.cjs
+++ slidev/.eslintrc.cjs
@@ -2,12 +2,12 @@
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting",
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+};
